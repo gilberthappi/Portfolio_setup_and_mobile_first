a252bd57ef4b5e7c5da0903cf878fb80881004f8
gilberthappi <gdushimimana6@gmail.com>
2023-07-25 21:20:46 +0200

    Fix Eslint error

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const humburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('navLinks');
-humburger.addEventListener('click',()=>{
-    navLinks.classList.toggle('show');
+humburger.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
 });
-navLinks.addEventListener('click',(event)=>{
-    if(navLinks.classList.contains('show')){
-        if(event.target.tagName==='A'||event.target.tagName==='I'){
-            navLinks.classList.remove('show');
-        }
+navLinks.addEventListener('click', (event) => {
+  if (navLinks.classList.contains('show')) {
+    if (event.target.tagName === 'A' || event.target.tagName === 'I') {
+      navLinks.classList.remove('show');
     }
+  }
 });
